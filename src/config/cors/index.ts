25605a96a8436237e6f3f8e7c720d76fbaa91cac
dlxkhang <khangduong0902@gmail.com>
2022-12-04 13:32:57 +0700

    add cors

--- src/config/cors/index.ts
+++ src/config/cors/index.ts
@@ -3,6 +3,7 @@ export const corsOptions = {
         'http://localhost:3000',
         'https://realtime-learning-fe.vercel.app',
         'https://realtime-learning-fe-midterm.vercel.app',
+        'https://realtime-learning-fe-ga02.vercel.app',
     ],
     methods: 'GET, POST, PUT, DELETE',
     credentials: true,
