e6560d83ea29a9498f414e5ad7c160fe251f10b5
dlxkhang <khangduong0902@gmail.com>
2022-12-04 16:16:27 +0700

    add cors

--- src/config/cors/index.ts
+++ src/config/cors/index.ts
@@ -5,6 +5,7 @@ export const corsOptions = {
         'https://realtime-learning-fe-midterm.vercel.app',
         'https://realtime-learning-fe-ga02.vercel.app',
         'https://realtime-learning-fe.onrender.com',
+        'https://realtime-learning-fe-ga02.netlify.app',
     ],
     methods: 'GET, POST, PUT, DELETE',
     credentials: true,
